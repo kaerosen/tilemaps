71ec0f495aa3ad9e45df5a2f6140b3941ea1e355
Kaelyn Rosenberg <kaerosenberg@gmail.com>
2020-04-13 16:10:54 -0700

    add check that range contains correct tile size

--- R/fit_tiles.R
+++ R/fit_tiles.R
@@ -1,6 +1,6 @@
 # STEP 3 - Fit tiles to boundary
 
-fit_tiles <- function(data, square = TRUE) {
+fit_tiles <- function(data, square = TRUE, flat_topped = FALSE) {
   boundary_poly <- st_union(data)
   boundary_line <- st_boundary(boundary_poly)
   original_centroids <- st_centroid(data)
@@ -11,7 +11,8 @@ fit_tiles <- function(data, square = TRUE) {
   s <- as.numeric(sqrt(A/R))
 
   # create grid
-  grid <- st_make_grid(boundary_poly, cellsize = s, square = square)
+  grid <- st_make_grid(boundary_poly, cellsize = s,
+                       square = square, flat_topped = flat_topped)
 
   # calculate number of tile centroids inside boundary
   tile_centroids <- st_centroid(grid)
@@ -22,19 +23,42 @@ fit_tiles <- function(data, square = TRUE) {
   if (num_contained > R) {
     s_range[1] <- s
     s_range[2] <- 1.1*s
+    grid <- st_make_grid(boundary_poly, cellsize = s_range[2],
+                         square = square, flat_topped = flat_topped)
+    tile_centroids <- st_centroid(grid)
+    new_num_contained <- length(st_contains(boundary_poly, tile_centroids)[[1]])
+    while(new_num_contained > R) {
+      s_range[2] <- 1.1*s_range[2]
+      grid <- st_make_grid(boundary_poly, cellsize = s_range[2],
+                           square = square, flat_topped = flat_topped)
+      tile_centroids <- st_centroid(grid)
+      new_num_contained <- length(st_contains(boundary_poly, tile_centroids)[[1]])
+    }
   } else {
     s_range[1] <- .9*s
     s_range[2] <- s
+    grid <- st_make_grid(boundary_poly, cellsize = s_range[1],
+                         square = square, flat_topped = flat_topped)
+    tile_centroids <- st_centroid(grid)
+    new_num_contained <- length(st_contains(boundary_poly, tile_centroids)[[1]])
+    while(new_num_contained < R) {
+      s_range[1] <- .9*s_range[1]
+      grid <- st_make_grid(boundary_poly, cellsize = s_range[1],
+                           square = square, flat_topped = flat_topped)
+      tile_centroids <- st_centroid(grid)
+      new_num_contained <- length(st_contains(boundary_poly, tile_centroids)[[1]])
+    }
   }
 
   # resize grid
   iter <- 1
-  print(c(iter, num_contained))
+  #print(c(iter, num_contained))
   while (num_contained != R) {
     if (iter > 50) {
       stop("failed to converge")
     }
-    grid <- st_make_grid(boundary_poly, cellsize = mean(s_range), square = square)
+    grid <- st_make_grid(boundary_poly, cellsize = mean(s_range),
+                         square = square, flat_topped = flat_topped)
     tile_centroids <- st_centroid(grid)
     num_contained <- length(st_contains(boundary_poly, tile_centroids)[[1]])
     iter <- iter + 1
