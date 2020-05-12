a7059cb7eace6b496373bf761c8ba1ca832be93b
Kaelyn Rosenberg <kaerosenberg@gmail.com>
2020-05-12 15:27:33 -0500

    add clue::

--- R/assign_regions.R
+++ R/assign_regions.R
@@ -3,5 +3,5 @@
 assign_regions <- function(region_centroids, tile_centroids) {
   dist_matrix <- matrix(as.numeric(st_distance(tile_centroids, region_centroids)), nrow = length(region_centroids))^2
   # output permutation of original indices
-  as.numeric(solve_LSAP(dist_matrix))
+  as.numeric(clue::solve_LSAP(dist_matrix))
 }
